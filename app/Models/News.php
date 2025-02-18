5d1cd28876193e004e14d6dda4b8f528d8fc54a8
iAlijon <Alijon.Sh098@gmail.com>
2025-02-18 14:34:04 +0500

    last

--- app/Models/News.php
+++ app/Models/News.php
@@ -18,7 +18,7 @@ class News extends Model
 
     public function getContentAttribute($value)
     {
-        $appUrl = config('app.url') . '/public/';
+        $appUrl = config('app.url').'/';
          $img = preg_replace_callback('/(<img\s+[^>]*src=")\/?(uploads\/[^"]+)(")/i', function ($matches) use ($appUrl) {
              // $matches[1] – rasmning nisbiy yoʻli: "uploads/images/example.jpg"
              return '<img src="' . $appUrl . $matches[2] . '"';

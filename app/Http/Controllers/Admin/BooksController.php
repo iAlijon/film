9ce2df104e75e71b93d44b70076b3c49df6c2f46
iAlijon <Alijon.Sh098@gmail.com>
2025-02-24 16:05:55 +0500

    last

--- app/Http/Controllers/Admin/BooksController.php
+++ app/Http/Controllers/Admin/BooksController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\BooksRequest;
-use App\Models\Bookgroup;
 use App\Models\PersonCategory;
 use App\Repositories\BooksRepository;
 use Illuminate\Http\Request;

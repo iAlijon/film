2bf8adcddf3bec81492027e500c071bd30ee738c
iAlijon <Alijon.Sh098@gmail.com>
2025-03-04 14:30:51 +0500

    last

--- resources/views/admin/filmography/index.blade.php
+++ resources/views/admin/filmography/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{route('filmography.index')}}">Home</a></li>
-                        <li class="breadcrumb-item active">artistic</li>
+                        <li class="breadcrumb-item active">Filmography</li>
                     </ol>
                 </div>
             </div>

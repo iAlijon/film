27fe2a8de6ca6dbc403b3492c454e8cfd3868199
iAlijon <Alijon.Sh098@gmail.com>
2025-08-19 10:26:00 +0500

    last

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -137,10 +137,10 @@ class TelegramController extends Controller
                         $content = $model['content_oz'];
                         $allowed = '<b><i><u><s><a><code><pre><strong><em><del><span class="tg-spoiler">';
                         $description = strip_tags($description, $allowed);
-                        $longDesc = mb_substr($description, 0, 700);
+                        $longDesc = mb_substr($description, 0, 300);
                         $remDesc = mb_substr($description, 1024);
                         $content = strip_tags($content, $allowed);
-                        $longCont = mb_substr($content, 0, 700);
+                        $longCont = mb_substr($content, 0, 600);
                         $remaining = mb_substr($content, 1024);
                         $caption = <<<TEXT
                         🎬: $name

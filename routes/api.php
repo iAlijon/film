fb7b7a8fa232db2e4bcc07a93baa90aa663635f2
iAlijon <Alijon.Sh098@gmail.com>
2025-08-29 12:11:32 +0500

    last

--- routes/api.php
+++ routes/api.php
@@ -37,22 +37,12 @@ Route::post('/telegram/webhook', [\App\Http\Controllers\TelegramController::clas
 
 Route::group(['middleware' => 'ApiAuth'], function (){
     Route::get('aphorism', [\App\Http\Controllers\AphorismController::class, 'index'])->name('aphorism');
-//    Route::get('news', [\App\Http\Controllers\NewsController::class, 'index'])->name('news.api.index');
     Route::get('news/{id}', [\App\Http\Controllers\NewsController::class, 'show'])->name('news.api.show')->middleware('view_count');
-    Route::get('premiere', [\App\Http\Controllers\PremiereController::class, 'index'])->name('premiere.api.index');
     Route::get('premiere/{id}', [\App\Http\Controllers\PremiereController::class, 'show'])->name('premiere.api.show');
-    Route::get('movie_analysis', [\App\Http\Controllers\FilmAnalysisController::class, 'index'])->name('movie_analysis.api.index');
     Route::get('movie_analysis/{id}', [\App\Http\Controllers\FilmAnalysisController::class,'show'])->name('movie_analysis.api.show');
-    Route::get('interview', [\App\Http\Controllers\InterviewController::class, 'index'])->name('interview.api.index');
     Route::get('interview/{id}', [\App\Http\Controllers\InterviewController::class, 'show'])->name('interview.api.show');
-    Route::get('persons', [\App\Http\Controllers\PersonDirectorController::class, 'index'])->name('person.api.index');
     Route::get('persons/{id}', [\App\Http\Controllers\PersonDirectorController::class, 'show'])->name('person.api.show');
-    Route::get('letters_category', [\App\Http\Controllers\DictionaryController::class, 'letters'])->name('letters');
-    Route::get('dictionary', [\App\Http\Controllers\DictionaryController::class, 'index'])->name('dictionary.api.index');
     Route::get('dictionary/{id}', [\App\Http\Controllers\DictionaryController::class, 'show'])->name('dictionary.api.show');
-    Route::get('cinema_fact', [\App\Http\Controllers\FilmFactController::class, 'index'])->name('cinema.api.index');
-    Route::get('filmography', [\App\Http\Controllers\FilmographyController::class, 'index'])->name('filmography.api.index');
     Route::get('filmography/{id}', [\App\Http\Controllers\FilmographyController::class, 'show'])->name('filmography.api.show');
-    Route::get('book', [\App\Http\Controllers\BookController::class, 'index'])->name('book.api.index');
     Route::get('book/{id}', [\App\Http\Controllers\BookController::class, 'show'])->name('book.api.show');
 });

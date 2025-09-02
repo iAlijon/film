d5a6f92ef177c813fa817084d0f87967e30b48ca
iAlijon <Alijon.Sh098@gmail.com>
2025-09-02 14:33:03 +0500

    last

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -148,11 +148,10 @@ class TelegramController extends Controller
                     $url = explode('/', $model['images']);
                     $last = array_pop($url);
                     $image_path = storage_path('app/public/premiere/'.$last);
-                    $id = $model['id'];
                     $keyboard = Keyboard::make()->inline()->row([
                         Keyboard::inlineButton([
                             'text' => "Davomini o‘qish",
-                            'url'  => "https://film-front-javohirs-projects-cf013492.vercel.app/premiere/{$id}"
+                            'url'  => "https://film-front-javohirs-projects-cf013492.vercel.app/premiere/{$model['id']}"
                         ])
                     ]);
                     Log::info($keyboard);

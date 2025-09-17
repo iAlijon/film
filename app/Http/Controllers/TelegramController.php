3555249e651fd701c9b3c6126bc81772a619a13c
iAlijon <Alijon.Sh098@gmail.com>
2025-09-17 10:29:49 +0500

    last

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -59,8 +59,7 @@ class TelegramController extends Controller
                     ->row(['Kino tahlil', 'Suhbatlar'])
                     ->row(['Shaxsiyat', 'Kinolug\'at'])
                     ->row(['Kinofakt', 'Filmografiya'])
-                    ->row(['Kitoblar'])
-                    ->row(['Baxolash']);
+                    ->row(['Kitoblar']);
                 $this->sendMessage($chat_id,'🖐 Bo\'timizga xush kelibsiz'.' '.$name,$keyboard);
             }elseif ($message === 'Yangiliklar') {
                 $models = News::where('status', 1)

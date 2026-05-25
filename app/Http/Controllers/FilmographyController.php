64dd0be3f98d74a5d36b7d94def8ec00169b6814
iAlijon <93911401+iAlijon@users.noreply.github.com>
2026-05-25 15:33:22 +0500

    last

--- app/Http/Controllers/FilmographyController.php
+++ app/Http/Controllers/FilmographyController.php
@@ -11,30 +11,6 @@ class FilmographyController extends Controller
 {
     public function index(Request $request)
     {
-//        $lang = $request->header('lang', 'oz');
-//        $result = $request->all();
-//        $per_page = $result['per_page']??6;
-//        if (isset($result['category_id']) && !empty($result['category_id'])) {
-//            $params = Filmography::where('category_id', $result['category_id'])->where('status', 1)
-//                ->with(['translations' => function ($q) use ($lang){
-//                    $q->where('translates' ,$lang);
-//                }])
-//                ->orderBy('created_at', 'desc')
-//                ->paginate($per_page);
-//        }else {
-//            $params = Filmography::where('status', 1)
-//                ->with(['translations' => function ($q) use ($lang){
-//                    $q->where('translates' ,$lang);
-//                }])
-//                ->orderBy('created_at', 'desc')
-//                ->paginate($per_page);
-//        }
-//
-//
-//        if ($params) {
-//            return successJson($params, 'ok');
-//        }
-//        return errorJson('Undefined Element !', 404);
         $lang = $request->header('lang', 'oz');
         $result = $request->all();
         $per_page = $result['per_page'] ?? 6;
@@ -56,13 +32,9 @@ class FilmographyController extends Controller
             $translate = $item->translations->first();
             $arr = $item->toArray();
             unset($arr['translations']);
-
-            if ($translate) {
-                $arr['name'] = $translate->name;
-                $arr['description'] = $translate->description;
-                $arr['content'] = $translate->content;
-            }
-
+            $arr['name'] = $translate->name ?? null;
+            $arr['description'] = $translate->description ?? null;
+            $arr['content'] = $translate->content ?? null;
             return $arr;
         });
 

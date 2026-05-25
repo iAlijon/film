6bfdf798fb6784a7ac58c8285432d87fc83ef88a
iAlijon <93911401+iAlijon@users.noreply.github.com>
2026-05-25 15:41:33 +0500

    last

--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,7 @@ Route::get('aphorism', [\App\Http\Controllers\AphorismController::class, 'index'
 Route::get('film_digests/{id}', [\App\Http\Controllers\PremiereController::class, 'show'])->name('film_digest.api.show');
 Route::get('film_diagnostics/{id}', [\App\Http\Controllers\FilmAnalysisController::class,'show'])->name('film_diagnostics.api.show');
 Route::get('film_catalogs/{id}', [\App\Http\Controllers\FilmographyController::class, 'show'])->name('film_catalogs.api.show');
-Route::get('film_grids/{id}', [\App\Http\Controllers\KinogidController::class, 'show'])->name('movie_guide.api.show');
+Route::get('film_grids/{id}', [\App\Http\Controllers\KinogidController::class, 'show'])->name('film_grids.api.show');
 Route::get('books/{id}', [\App\Http\Controllers\BookController::class, 'show'])->name('book.api.show');
 
 //Route::get('news/{id}', [\App\Http\Controllers\NewsController::class, 'show'])->name('news.api.show')->middleware('view_count');

6f31c9b06a36dea59fe1e0bb08ba384ef0440685
iAlijon <93911401+iAlijon@users.noreply.github.com>
2026-05-25 11:05:49 +0500

    last

--- app/Traits/ImageUploads.php
+++ app/Traits/ImageUploads.php
@@ -11,12 +11,12 @@ trait ImageUploads
 {
     public function uploads($image, $folder)
     {
-        $app_url = config('app.url');
+//        $app_url = config('app.url');
         $dir_name = 'public/'.$folder;
         $file_name = time().'_'.$image->getClientOriginalName();
         $path = $dir_name."/".$file_name;
         if (Storage::put($path, file_get_contents($image))) {
-            return $app_url.'/storage/'.$folder.'/'.$file_name;
+            return '/storage/'.$folder.'/'.$file_name;
         }else{
             return null;
         }
@@ -24,12 +24,12 @@ trait ImageUploads
 
     public function fileUploads($file, $folder)
     {
-        $app_url = config('app.url');
+//        $app_url = config('app.url');
         $file_name = time().'_'.$file->getClientOriginalName();
         $file_name = str_replace(' ', '_', $file_name);
         $path = public_path('files/').$folder;
         if ($file->move($path, $file_name)){
-            return $app_url.'/files/'.$folder.'/'.$file_name;
+            return '/files/'.$folder.'/'.$file_name;
         }else{
             return null;
         }

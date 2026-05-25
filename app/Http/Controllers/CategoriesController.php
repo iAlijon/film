e37c452d4741f6584e3fea6ff7dc4677027917a9
iAlijon <93911401+iAlijon@users.noreply.github.com>
2026-05-25 15:51:03 +0500

    last

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,13 +9,6 @@ class CategoriesController extends Controller
 {
     public function index(Request $request)
     {
-//        $lang = $request->header('lang', 'oz');
-//        $categories = PersonCategory::where('status', 1)->with(['translates' => function ($q) use ($lang){
-//            $q->where('translates', $lang);
-//        }])
-//            ->orderBy('created_at', 'asc')
-//            ->get();
-//        return response()->json(['success' => true, 'data' => $categories, 'message' => 'ok']);
         $lang = $request->header('lang', 'oz');
 
         $categories = PersonCategory::where('status', 1)

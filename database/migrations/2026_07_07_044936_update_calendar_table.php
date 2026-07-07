9dc9149d90456feee735d9f2d57dce2c2b3d02bd
iAlijon <93911401+iAlijon@users.noreply.github.com>
2026-07-07 17:00:55 +0500

    last

--- database/migrations/2026_07_07_044936_update_calendar_table.php
+++ database/migrations/2026_07_07_044936_update_calendar_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
         Schema::table('calendars', function (Blueprint $table){
             $table->string('date');
             $table->boolean('status');
+            $table->integer('order');
         });
     }
 

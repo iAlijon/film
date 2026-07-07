30ee40c614de4c562c6f16903fddae0638568462
iAlijon <93911401+iAlijon@users.noreply.github.com>
2026-07-07 18:05:20 +0500

    last

--- database/migrations/2026_05_08_064246_create_kinogit_translatins_table.php
+++ database/migrations/2026_05_08_064246_create_kinogit_translatins_table.php
@@ -11,27 +11,27 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('kinogit_translatins', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('kinogit_id')->constrained()->cascadeOnDelete();
-            $table->string('name');
-            $table->string('description', 10000);
-            $table->string('image');
-            $table->text('content');
-            $table->string('translates', 2);
-            $table->timestamps();
-        });
-    }
+//    public function up()
+//    {
+//        Schema::create('kinogit_translatins', function (Blueprint $table) {
+//            $table->id();
+//            $table->foreignId('kinogit_id')->constrained()->cascadeOnDelete();
+//            $table->string('name');
+//            $table->string('description', 10000);
+//            $table->string('image');
+//            $table->text('content');
+//            $table->string('translates', 2);
+//            $table->timestamps();
+//        });
+//    }
 
     /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::dropIfExists('kinogit_translatins');
-    }
+//    public function down()
+//    {
+//        Schema::dropIfExists('kinogit_translatins');
+//    }
 };
